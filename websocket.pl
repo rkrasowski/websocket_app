d814f65b8d9dee5bdc764b712b1b8ecdc57d41a9
Robert Krasowski <rkrasowski@yahoo.com>
2015-06-20 19:12:38 +0000

    Working with scripts in public

---
+++ websocket.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+any '/' => 'index';
+websocket '/data' => sub {
+  my $self = shift;
+  my $timer = Mojo::IOLoop->recurring( 1 => sub {
+    state $i = 0;
+    $self->send({ json => gen_data($i++) });
+  });
+
+  $self->on( finish => sub {
+    Mojo::IOLoop->remove($timer);
+  });
+};
+
+sub gen_data {
+  my $x = shift;
+  return [ $x, sin( $x + 2*rand() - 2*rand() ) ]
+}
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+
+% layout 'basic';
+
+
+
+%= javascript '/jquery-1.11.3.min.js'
+%= javascript '/jquery.flot.js'
+
+<div id="plot" style="width:600px;height:300px">
+</div>
+
+%= javascript begin
+  var data = [];
+  var plot = $.plot($('#plot'), [ data ]);
+
+  var url = '<%= url_for('data')->to_abs %>';
+  var ws = new WebSocket( url );
+  ws.onmessage = function(e){
+    var point = JSON.parse(e.data);
+    data.push(point);
+    plot.setData([data]);
+    plot.setupGrid();
+    plot.draw();
+  };
+% end
